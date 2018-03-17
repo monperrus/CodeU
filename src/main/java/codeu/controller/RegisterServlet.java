2e35c22df5ba4aef177dcd719f7374f6b3821c0b
isabellasteains <isabella.steains@gmail.com>
2018-03-17 08:29:34 -0400

    Register Servlet files...

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -6,17 +6,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
-* Servlet class responsible for user registration.
-*/
-
+ * Servlet class responsible for user registration.
+ */
 public class RegisterServlet extends HttpServlet {
 
  @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
      throws IOException, ServletException {
 
-   response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
+   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
  }
 }
