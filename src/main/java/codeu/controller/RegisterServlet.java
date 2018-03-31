f8f923284587e6e8959e916345fee11284ab8be6
Juliana Sales <jsales@seas.upenn.edu>
2018-03-31 17:34:03 -0400

    added do post to register servlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -10,12 +10,21 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet class responsible for user registration.
  */
 public class RegisterServlet extends HttpServlet {
-
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-     throws IOException, ServletException {
-
-   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
-   System.out.println();
- }
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+    }
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        response.getWriter().println("<p>Username: " + username + "</p>");
+        response.getWriter().println("<p>Password: " + password + "</p>");
+    }
 }
