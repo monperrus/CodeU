d380aff5579ede93da10114b3bd75bdc04a7c233
Juliana Sales <jsales@seas.upenn.edu>
2018-03-31 18:21:39 -0400

    changes to register servlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -11,6 +11,29 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RegisterServlet extends HttpServlet {
     
+    /**
+     * Store class that gives access to Users.
+     */
+    private UserStore userStore;
+    
+    /**
+     * Set up state for handling registration-related requests. This method is only called when
+     * running in a server, not when running in a test.
+     */
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        setUserStore(UserStore.getInstance());
+    }
+    
+    /**
+     * Sets the UserStore used by this servlet. This function provides a common setup method
+     * for use by the test framework or the servlet's init() function.
+     */
+    void setUserStore(UserStore userStore) {
+        this.userStore = userStore;
+    }
+    
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
@@ -24,7 +47,21 @@ public class RegisterServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         
-        response.getWriter().println("<p>Username: " + username + "</p>");
-        response.getWriter().println("<p>Password: " + password + "</p>");
+        if (!username.matches("[\\w*\\s*]*")) {
+            request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+            request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+            return;
+        }
+        
+        if (userStore.isUserRegistered(username)) {
+            request.setAttribute("error", "That username is already taken.");
+            request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+            return;
+        }
+        
+        User user = new User(UUID.randomUUID(), username, password, Instant.now());
+        userStore.addUser(user);
+        
+        response.sendRedirect("/login");
     }
 }
