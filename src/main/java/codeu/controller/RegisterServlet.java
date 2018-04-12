f76fd53085e90b4adb8cd64a1b4e1a474b18edc6
isabellasteains <34217378+isabellasteains@users.noreply.github.com>
2018-04-12 16:36:46 -0400

    Bug Fix for RegisterServlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import codeu.model.store.*;
 
 /**
  * Servlet class responsible for user registration.
